50afa22a805b346b21cf16f3b8a1ac08063295bc
Mor Soferian <47902941+MorSof@users.noreply.github.com>
2020-04-25 15:55:02 +0300

    Initial Commit

---
+++ Memory Game IOS V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory Game IOS V2
+//
+//  Created by Mor Soferian on 4/25/20.
+//  Copyright © 2020 Mor Soferian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Memory Game IOS V2Tests/Memory_Game_IOS_V2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Memory_Game_IOS_V2Tests.swift
+//  Memory Game IOS V2Tests
+//
+//  Created by Mor Soferian on 4/25/20.
+//  Copyright © 2020 Mor Soferian. All rights reserved.
+//
+
+import XCTest
+@testable import Memory_Game_IOS_V2
+
+class Memory_Game_IOS_V2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
